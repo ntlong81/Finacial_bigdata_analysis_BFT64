bebee654c2a871a4d4a427e0b6c8fec6310239cb
Nguyen Tuan Long <60863790+ntlong81@users.noreply.github.com>
2025-02-27 14:30:47 +0700

    2702

---
+++ Data/my_module_2.py
@@ -0,0 +1,28 @@
+def is_prime(n):
+    if n<=1:
+        return False
+    else:
+        for d in range(2,int(n**0.5)+1):
+            if n%d==0:
+                return False   
+    return True
+
+def normalize(text):
+    text_norm = text.lower()
+    special_letters = {'.',',','-','*','!',"?",':'}
+    for letter in special_letters:
+        text_norm = text_norm.replace(letter,' ')
+    while '  ' in text_norm:
+        text_norm = text_norm.replace("  "," ")
+    return text_norm
+def get_fre_word(text):
+    fre_dict = {}
+    for word in text.split():
+        if word in fre_dict:
+            fre_dict[word] += 1
+        else:
+            fre_dict[word] = 1
+    return fre_dict
+print('Well come to my module 2')
+
+big_digit = 24248203840243802438
