ea043afab7067acc02070340cd57cad0b92d5f7f
Nguyen Tuan Long <60863790+ntlong81@users.noreply.github.com>
2025-02-27 13:59:04 +0700

    2702

--- code_visualization.py
+++ code_visualization.py
@@ -6,3 +6,13 @@ for word in words:
     else:
         fre_word[word] = 1
 print(fre_word)
+
+def is_prime(n):
+    if n<=1:
+        return False
+    else:
+        for d in range(2,int(n**0.5)+1):
+            if n%d==0:
+                return False   
+    return True
+is_prime(29)
